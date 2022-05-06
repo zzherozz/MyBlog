2833c7742f866d58b8cd823c9d59affda6727813
宾洁 <jie.bin@chiefclouds.com>
2022-05-06 16:10:44 +0800

    feat: l胡乱调整

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,8 @@ const BASE_URL = '/MyBlog/';
 // const isPro = process.env.NODE_ENV === 'production';
 // const baseUrl = isPro ? BASE_URL : '/';
 export default defineConfig({
-  // base: BASE_URL,
-  // publicPath: BASE_URL,
+  base: BASE_URL,
+  publicPath: BASE_URL,
   title: 'myblog',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
