02b29935ebd9862594b31bbebc6fdaf9958343b6
宾洁 <jie.bin@chiefclouds.com>
2022-06-13 18:31:37 +0800

    feat: 底部加载

--- src/packages/dailyRecord/virtuaList/index copy.jsx
+++ src/packages/dailyRecord/virtuaList/index copy.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useState } from 'react';
-import { useInfiniteScroll } from 'ahooks';
-
+import React, { useRef, useState, useEffect } from 'react';
+import { isFunction, throttle } from 'lodash-es';
 const resultData = [
+  '0',
   '1',
   '2',
   '3',
@@ -23,59 +23,73 @@ const resultData = [
   '19',
   '20',
   '21',
+  '22',
 ];
-function getLoadMoreList(page, pageSize) {
-  console.log(page);
-  const start = (page - 1) * pageSize;
-  const end = page * pageSize;
-  const list = resultData.slice(start, end);
-  const nId = resultData.length >= end ? resultData[end] : undefined;
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        list,
-        nextId: nId,
-        total: resultData.length,
-      });
-    }, 1000);
+const VirtuaList = () => {
+  const [list, setList] = useState([]);
+  const [pageConfig, setPageConfig] = useState({
+    pageNo: 1,
+    pageSize: 10,
   });
-}
-const PAGE_SIZE = 10;
-const VirtuaList = (props) => {
-  const { data, loading, loadMore, loadingMore, noMore } = useInfiniteScroll(
-    (d) => {
-      const page = d ? Math.ceil(d.list.length / PAGE_SIZE) + 1 : 1;
-      return getLoadMoreList(page, PAGE_SIZE);
-    },
-    {
-      target: ref,
-      isNoMore: (d) => d?.nextId === undefined,
-    },
-  );
-  const ref = useRef(null);
+  const currentRef = useRef(null);
+  useEffect(() => {
+    currentRef.current = pageConfig;
+  }, [pageConfig]);
+  // 获取数据的函数
+  const getMoreList = () => {
+    const { pageNo, pageSize } = currentRef.current;
+    // 事件监听这里：获取不到最新的pageConfig
+    // const {pageNo, pageSize} = pageConfig; 这里通过状态获取的pageConfig,函数外拿到的pageConfig已经不是同一个东西了
+    // 函数内的pageConfig在开始监听document.addEventListene的时候已经确定了。
+    const start = (pageNo - 1) * pageSize;
+    const end = pageNo * pageSize;
+    const tempList = resultData.slice(start, end);
+    if (pageNo <= Math.ceil(resultData?.length / pageSize)) {
+      setPageConfig((pageConfig) => ({ ...pageConfig, pageNo: pageNo + 1 }));
+      setList((prelist) => [...prelist, ...tempList]);
+      // 新的State要通过当前的state计算得出,使用函数形式。错误方式setList(([...list, ...tempList])),这里的list是[]。
+    }
+  };
+  // 判断是否触底
+  const isScrollToBottom = (handler) => {
+    const div = document.getElementById('scrollWrap');
+    const scrollTop = div.scrollTop; // 滚动条高度
+    const clientHeight = div.clientHeight; // 可视区域高度
+    const scrollHeight = div.scrollHeight; // 总高度---滚动部分高度
+    if (scrollTop + clientHeight >= scrollHeight) {
+      isFunction(handler) && handler();
+    }
+  };
+  //节流一波
+  const fn = throttle(() => {
+    isScrollToBottom(getMoreList);
+  }, 500);
+  useEffect(() => {
+    getMoreList(pageConfig.pageNo, pageConfig.pageSize);
+  }, []);
+
+  useEffect(() => {
+    // 开启侦听器,监听页面滚动
+    document.getElementById('scrollWrap').addEventListener('scroll', fn);
+    // 组件销毁时移除侦听器
+    return () => {
+      document.getElementById('scrollWrap').removeEventListener('scroll', fn);
+    };
+  }, []);
+
   return (
-    <div ref={ref} style={{ height: 300, overflow: 'auto', border: '1px solid', padding: 12 }}>
-      {loading ? (
-        <p>loading</p>
-      ) : (
-        <div>
-          {data?.list?.map((item) => (
-            <div key={item} style={{ padding: 12, border: '1px solid #f5f5f5' }}>
-              item-{item}
+    <div
+      style={{ height: 300, overflow: 'auto', border: '1px solid', padding: 12 }}
+      id="scrollWrap"
+    >
+      {Array.isArray(list) &&
+        list.map((item, index) => {
+          return (
+            <div key={index} style={{ padding: 12, border: '1px solid #f5f5f5' }}>
+              这里是：{item}
             </div>
-          ))}
-        </div>
-      )}
-
-      <div style={{ marginTop: 8 }}>
-        {!noMore && (
-          <button type="button" onClick={loadMore} disabled={loadingMore}>
-            {loadingMore ? 'Loading more...' : 'Click to load more'}
-          </button>
-        )}
-
-        {noMore && <span>No more data</span>}
-      </div>
+          );
+        })}
     </div>
   );
 };
--- src/packages/dailyRecord/virtuaList/index.md
+++ src/packages/dailyRecord/virtuaList/index.md
@@ -4,13 +4,40 @@ nav:
   path: /daily-record
 ---
 
-## react-window 虚拟滚动&触底加载 list
+## 触底加载 list
 
 Demo:
 
 ```tsx
 import React from 'react';
 import { VirtuaList } from 'myblog';
+
+export default () => <VirtuaList name="First Demo" />;
+```
+
+## 手写触底加载 demo
+
+### 思路(伪代码一波)
+
+1.使用 api 分别获取以下高度
+
+```js
+滚动高度（document.documentElement.scrollTop）
+可视区域/屏幕高度（document.documentElement.clientHeight）
+页面高度（document.documentElement.scrollHeight）
+```
+
+2.条件判断：
+
+```js
+If(滚动高度 + 可视区域  >= 页面高度){ do something 函数}
+```
+
+Demo:
+
+```tsx
+import React from 'react';
+import VirtuaList from './index copy.jsx';
 
 export default () => <VirtuaList name="First Demo" />;
 ```
