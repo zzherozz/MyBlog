bc5853c24e6399744d36eff996f6fc0e0d59c247
binjie <1427552987@qq.com>
2022-07-29 00:15:15 +0800

    feat:测试

--- src/packages/dailyRecord/antd/demo1.jsx
+++ src/packages/dailyRecord/antd/demo1.jsx
@@ -1,64 +1,113 @@
-import React, { useRef, useState, useMemo } from 'react';
-import { Select, Form, Input } from 'antd';
+import { Form, Input, Table } from 'antd';
+import React, { useEffect } from 'react';
 // import {debounce} from 'loadsh'
-const SelectDemo = () => {
-  const [searchValue, setSearchValue] = useState();
-  const inputRef = useRef(null);
-  // function throttle(func, wait) {
-  //   let timer = null;
-  //   console.log('执行了');
-  //   return function () {
-  //     console.log(timer, 'timer');
-  //     const args = arguments;
-  //     const context = this;
-  //     if (!timer) {
-  //       timer = setTimeout(() => {
-  //         func.apply(context, args);
-  //         timer = null;
-  //       }, wait);
-  //     }
-  //   };
-  // }
-  function debounce(func, wait) {
-    const args = arguments;
-    const context = this;
-    let timer = null;
-    console.log('执行了', timer);
-    return function () {
-      if (timer) clearTimeout(timer);
-      console.log(timer, 'timer');
-      timer = setTimeout(function () {
-        func.apply(context, args);
-      }, wait);
-    };
-  }
+const { Item, useForm } = Form;
 
-  function throttle(func, wait) {
-    let temp = 0; // 这玩意存在闭包,所以只声明了一次。
-    console.log('执行了');
-    return function () {
-      console.log(temp, 'temp');
-      const now = Date.now();
-      const context = this;
-      const args = arguments;
-      if (now - temp > wait) {
-        func.apply(context, args);
-        temp = now;
-      }
-    };
-  }
-  const onInputChange = () => {
-    const value = inputRef?.current?.input?.value;
-    setSearchValue(value);
-    console.log(value, 'value', inputRef);
+const SelectDemo = () => {
+  const [form] = Form.useForm();
+  useEffect(() => {
+    form.setFieldsValue({
+      attributes: [
+        {
+          property: '授权状态1',
+          attribute: '联系方式当前授权状态2',
+        },
+        {
+          property: '授权状态3',
+          attribute: '联系方式当前授权状态3',
+        },
+      ],
+    });
+  }, []);
+
+  const renderColumns = (fields, add, remove) => {
+    const columns = [
+      {
+        title: '属性',
+        dataIndex: 'property',
+        key: 'property',
+        render: (text, record, index) => {
+          console.log(text, record, index, '3333');
+          // return (
+          //   <Item name={[index, 'property']}>
+          //     <Input placeholder="支持中英文字符输入" autoComplete="off" />
+          //   </Item>
+          // );
+          if (!record.isChildren) {
+            return (
+              <Item name={[index, 'property']}>
+                <Input placeholder="支持中英文字符输入" autoComplete="off" />
+              </Item>
+            );
+          } else {
+            return (
+              <Item name={[record.preIndex, 'children', index, 'property']}>
+                <Input placeholder="支持中英文字符输入" autoComplete="off" />
+              </Item>
+            );
+          }
+        },
+      },
+      {
+        title: '数据映射',
+        dataIndex: 'attribute',
+        key: 'attribute',
+        render: (text, record, index) => {
+          console.log(text, record, index, '3333');
+          if (!record.isChildren) {
+            return (
+              <Item name={[index, 'attribute']}>
+                <Input placeholder="支持中英文字符输入" autoComplete="off" />
+              </Item>
+            );
+          } else {
+            return (
+              <Item name={[record.preIndex, 'children', index, 'attribute']}>
+                <Input placeholder="支持中英文字符输入" autoComplete="off" />
+              </Item>
+            );
+          }
+        },
+      },
+      {
+        title: '操作',
+        dataIndex: 'action',
+        key: 'action',
+        render: (text, record, index) => {
+          return (
+            <Form.List name={[index, 'children']} noStyle>
+              {(fieldItems, { add, remove }) => (
+                <div onClick={() => add({ isChildren: true, preIndex: index })}>添加</div>
+              )}
+            </Form.List>
+          );
+
+          // return <div onClick={()=>{
+          //   form.setFields([{
+          //     name:['attributes',index,'children'],
+          //     value:[{isChildren:true}]
+          //   }])
+          // }}>添加</div>;
+        },
+      },
+    ];
+    return columns;
   };
-  // const onThrottleInputChange = debounce(onInputChange, 1000);
-  const onThrottleInputChange = useMemo(() => debounce(onInputChange, 2000), []);
   return (
-    <>
-      <Input type="text" onChange={onThrottleInputChange} ref={inputRef} />
-      <div>搜索的值：{searchValue}</div>
-    </>
+    <Form form={form}>
+      {/* <Item name='attributes'></Item> */}
+      <Form.List name="attributes">
+        {(fields, { add, remove }) => (
+          <Item noStyle shouldUpdate>
+            {({ getFieldValue }) => {
+              const data = getFieldValue('attributes');
+              console.log(data, 'data');
+              return <Table dataSource={data} columns={renderColumns()}></Table>;
+            }}
+          </Item>
+        )}
+      </Form.List>
+    </Form>
   );
 };
 export default SelectDemo;
