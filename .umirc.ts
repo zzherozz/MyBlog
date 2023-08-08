b42662937bccb3ae5d2799be343b6cbc09a903df
binjie <1427552987@qq.com>
2023-08-08 16:23:27 +0800

    feat:修改封面描述

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   base: '/MyBlog',
   publicPath: '/MyBlog/',
-  title: '蓝胖子的网络笔记',
+  title: '宾胖子的网络笔记',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
